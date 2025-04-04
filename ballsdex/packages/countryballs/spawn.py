af67c2c20accb6b36879f954600df7c2cb9c569b
ZenithDramill <steelerob27@gmail.com>
2025-04-04 12:11:52 -0400

    Update spawn.py

--- ballsdex/packages/countryballs/spawn.py
+++ ballsdex/packages/countryballs/spawn.py
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 
 log = logging.getLogger("ballsdex.packages.countryballs")
 
-SPAWN_CHANCE_RANGE = (40, 55)
+SPAWN_CHANCE_RANGE = (8, 16)
 
 CachedMessage = namedtuple("CachedMessage", ["content", "author_id"])
 
